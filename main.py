786f48e8128a4147f2917d69f9058d6071f7a8b1
GameCobra <adrian@houser.ca>
2024-03-08 10:54:06 -0500

    Inital Commit

---
+++ main.py
@@ -0,0 +1,53 @@
+class Algebra:
+    equationList = []
+
+    def __init__(self, equation: str) -> None:
+        self.equation = equation
+
+    #converts the equation atribute of the class to the equationList to allow for easyer usage latter
+    #seperates each charecter in the equation to a different element in equationList
+    def StringToList(self):
+        for i in range(len(self.equation)):
+            self.equationList.append(self.equation[i])
+
+    
+    #designed to be proceaded by the StringToList function
+    #Searchs through equationList and removes all space elements
+    #Dose this simply by going through every element in the list and removng it if it is a spaced
+    def RemoveSpace(self):
+        i = 0
+        while i < len(self.equationList):
+            if self.equationList[i] == " ":
+                self.equationList.pop(i)
+                i -= 1
+            i += 1
+
+    #Designed to be proceaded by the RemoveSpace function
+    #will combine adgasent numbers in the equationList var to be a single number in a single element
+    def SplitEquation(self):
+        symbolsToLookFor = ["+", "-", "*", "/", "!", "=", "(", ")", "^"]
+        newEquationList = [""]
+        for i in range(len(self.equationList)):
+            for j in range(len(symbolsToLookFor)):
+                if self.equationList[i] == symbolsToLookFor[j] or self.equationList[i - 1] == symbolsToLookFor[j]:
+                    newEquationList.append("")
+            newEquationList[len(newEquationList) - 1] =  str(newEquationList[len(newEquationList) - 1]) + self.equationList[i]
+        self.equationList = newEquationList
+
+
+    #Surchs the equationList var for the provided symobol
+    #will return a list of all indexs where that symobol is pressent
+    #it will return [] if that symbol is not in the equationList
+    def DoseContainSymbol(self, symbolToSearchFor):
+        indexs = []
+        for i in range(len(self.equationList)):
+            if self.equationList[i] == symbolToSearchFor:
+                indexs.append(i)
+        return indexs
+
+
+a = Algebra("1 + 356 - 6")
+a.StringToList()
+a.RemoveSpace()
+a.SplitEquation()
+print(a.equationList)
