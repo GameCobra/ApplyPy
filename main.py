7051a0b964aa6a5682a2f797b2316c24fbfc208a
GameCobra <adrian@houser.ca>
2024-03-22 09:54:20 -0400

    DMAS part of BEDMAS

--- main.py
+++ main.py
@@ -38,16 +38,21 @@ class Algebra:
 
     #Surchs the equationList var for the provided symobol
     #will return a list of all indexs where that symobol is pressent
-    #it will return [] if that symbol is not in the equationList
+    #it will return -1 if that symbol is not in the equationList
     def DoseContainSymbol(self, symbolToSearchFor):
         for i in range(len(self.equationList)):
             if self.equationList[i] == symbolToSearchFor:
                 return i
-    
+        return -1
+
+    '''
     def ConvertStringNumbersToInts(self):
         for i in range(self.equationList):
-            for j in range(len(self.Operations)):
-                pass
+            for j in range(len(self.Operations) + 1):
+                if (self.equationList[i] == self.equationList):
+                    break
+                if (j == len(self.Operations)):
+    '''
 
     #Gose through the EquationList to find the first "+" and adds the values on eather side of it
     #Takes the combi ned value and replaces the 3 elements used with the new calculated value
@@ -81,15 +86,44 @@ class Algebra:
         self.equationList.pop(Indexs + 1)
         self.equationList.pop(Indexs - 1)
 
-    def Simplification(self):
-        pass    
+    #Dose the multiplication and division portion of BEDMAS
+    def MultAndDiv(self):
+        nextMult = self.DoseContainSymbol("*")
+        nextDiv = self.DoseContainSymbol("/")
+        if nextMult == -1 and nextDiv != -1:
+            self.Divistion()
+        elif nextMult != -1 and nextDiv == -1:
+            self.Multiplication()
+        elif nextMult < nextDiv:
+            self.Multiplication() 
+        elif nextDiv < nextMult:
+            self.Divistion()
+        else:
+            return
+        self.MultAndDiv()
+        return
+    
+    def AddAndSub(self):
+        nextAdd = self.DoseContainSymbol("+")
+        nextSub = self.DoseContainSymbol("-")
+        if nextAdd == -1 and nextSub != -1:
+            self.Subtraction()
+        elif nextAdd != -1 and nextSub == -1:
+            self.Addition()
+        elif nextAdd < nextSub:
+            self.Addition() 
+        elif nextSub < nextAdd:
+            self.Subtraction()
+        else:
+            return
+        self.AddAndSub()
+        return
+
 
 a = Algebra("7 + 2 * 10 - 6 / 2")
 a.StringToList()
 a.RemoveSpace()
 a.SplitEquation()
-a.Multiplication()
-a.Divistion()
-a.Addition()
-a.Subtraction()
+a.MultAndDiv()
+a.AddAndSub()
 print(a.equationList)
